8aa55ac2b5fea59ab569a69d12f3ecae02719b10
iccee0 <66622055+RoeHH@users.noreply.github.com>
2021-12-16 13:46:24 +0100

    d

--- routes/login.js
+++ routes/login.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('login');
 });
 
+router.post('/', function(req, res) {
+    async.waterfall([
+      function (callback) {
+        userCollection.findOne({
+          "email_address": req.body.email
+        }, function (err, result) {
+          if (err) {
+            console.log(err);
+            res.send({error: "An error has occurred"});
+          } else {
+            if (result == null) {
+              res.send({"error": "This email address is not recognised, please check you have entered your email correctly"});
+            } else {
+              console.log("Email recognised");
+              callback(null, result);
+            }
+          }
+        });
+      }
+    ])
+  });
+
 module.exports = router;
